83fca0a005d27a4a2bcb37cfabe96f5284828e29
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2024-11-17 18:16:05 +0000

    Add gitignore, rels and cm function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="modelviz",
-    version="0.1.2",
+    version="0.1.3",
     author="Gary Hutson",
     author_email="hutsons-hacks@engineer.com",
     description="A package for EDA and Sci-Kit Learn visualisations and utilities",
@@ -13,13 +13,17 @@ setup(
     install_requires=[
         "pandas>=1.0",        # For DataFrame operations
         "numpy>=1.20",        # For numerical computations
-        "scikit-learn>=0.24"  # For machine learning utilities
+        "scikit-learn>=0.24", # For machine learning utilities
+        "seaborn"
     ],
     extras_require={
         "dev": ["pytest>=7.0"]  # Development and testing dependencies
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
