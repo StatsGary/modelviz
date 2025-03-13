0c40abc3bafab6577d87db4d4ddd1c6d9a133393
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2025-03-13 10:38:32 +0000

    Moving to version 2.1.0 of modelviz

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup, find_packages
+"""Setup file to determine imports"""
 import re
-
+from setuptools import setup, find_packages
 
 def get_version():
-    with open("modelviz/__init__.py", "r") as f:
+    """Gets the current version of the package"""
+    with open("modelviz/__init__.py", "r", encoding='utf-8') as f:
         version_line = next((line for line in f if line.startswith("__version__")), None)
         if version_line:
             return re.search(r"\"(.*?)\"", version_line).group(1)
         raise RuntimeError("Version not found in modelviz/__init__.py")
 
+def read_requirements():
+    """Read dependencies from requirements.txt"""
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
 setup(
     name="modelviz",
@@ -16,18 +21,13 @@ setup(
     author="Gary Hutson",
     author_email="hutsons-hacks@outlook.com",
     description="A package for EDA and Sci-Kit Learn visualisations and utilities",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding='UTF-8').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/StatsGary/modelviz",
     packages=find_packages(),
-    install_requires=[
-        "pandas>=1.0",        # For DataFrame operations
-        "numpy>=1.20",        # For numerical computations
-        "scikit-learn>=0.24", # For machine learning utilities
-        "seaborn"
-    ],
+    install_requires=read_requirements(),
     extras_require={
-        "dev": ["pytest>=7.0"]  # Development and testing dependencies
+        "dev": ["pytest>=7.0"]  
     },
     classifiers=[
         "Programming Language :: Python :: 3.9",
@@ -39,4 +39,3 @@ setup(
     ],
     python_requires=">=3.9",
 )
-
